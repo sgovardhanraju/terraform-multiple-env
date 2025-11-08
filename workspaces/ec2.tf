90c97cb1b443992c565d20c894f5ca65948f29a2
Govardhan Raju <gsurapur@gmail.com>
2025-11-08 23:06:37 +0400

    session-33

--- workspaces/ec2.tf
+++ workspaces/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terraform" {
     ami = var.ami_id
-    instance_type = lookup(var.instance_type, terraform.workspace )
+    instance_type = lookup(var.instance_type, terraform.workspace)
     vpc_security_group_ids = [aws_security_group.allow_all.id]
     tags = merge(
       local.common_tags,
